f2fb38a4a24d9e3d44d3456e7256a1c07ee33c43
RiyankVaghasiya <vaghasiyariyank@gmail.com>
2025-06-21 16:04:08 +0530

    make some changes in setup file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name='DS-project',
+    name='Student-Performance-Prediction',
     version='0.0.1',
     author='RiyankVaghasiya',
     author_email='vaghasiyariyank@gmail.com',
